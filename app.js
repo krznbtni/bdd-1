ff2a1ac4a8969033618c77d1ab8ed6412758a918
DennisMajvall <dennis@majvall.se>
2017-09-05 14:34:01 +0200

    removed app.js content

--- app.js
+++
@@ -1,8 +0,0 @@
-
-// Import the GroceryListTest class
-let GroceryListTest = require('./grocery-list-test');
-let GroceryListItemTest = require('./grocery-list-item-test');
-
-// Run the tests
-new GroceryListTest();
-new GroceryListItemTest();
