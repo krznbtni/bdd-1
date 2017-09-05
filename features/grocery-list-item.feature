453ca3166b5b9f5b22068b0a99a23f984d8ebd92
DennisMajvall <dennis@majvall.se>
2017-09-05 17:04:01 +0200

    done with tests for feature: grocery-list-item

--- features/grocery-list-item.feature
+++ features/grocery-list-item.feature
@@ -2,26 +2,41 @@ Feature: Creating an grocery list item
   As a user
   I should be able to create and set the name, category and quantity of an item
 
-  Scenario: Failing to create an item with no name
-    Given that I have not entered a name for a new item
+
+
+  Scenario: Failing to create an item with an invalid name
+    Given that I have not entered a valid name for a new item
     When I try to create an item with that info
     Then no item should be created
-    And I should be notified that a name is needed to be supplied
+    And I should be notified that a valid name is needed to be supplied
 
-  Scenario: Failing to create an item with no quantity
-    Given that I have entered less than 1 as quantity
+  Scenario: Failing to create an item with an invalid category
+    Given that I have not entered a valid category for a new item
+    When I try to create an item with that info
+    Then no item should be created
+    And I should be notified that a valid category must be supplied
+
+  Scenario: Failing to create an item with an invalid quantity
+    Given that I have entered less than 1 as quantity for a new item
     When I try to create an item with that info
     Then no item should be created
     And I should be notified that a quantity of 1 or more must be supplied
 
+
   Scenario: Failing to set the name of an item
-    Given that I have not entered a name for an item
-    When I try to set the name of an item to that
+    Given that I have not entered a valid name for an existing item
+    When I try to set the "name" of the item to that
     Then nothing should happen to the item
     And I should be notified that a valid name is needed to be supplied
 
-  Scenario: Failing to set the quantity of an item
-    Given that I have entered a quantity of an item to be less than 1
-    When I try to set the quantity of an item to that
+  Scenario: Failing to set the category of an item
+    Given that I have not entered a valid category for an existing item
+    When I try to set the "category" of the item to that
     Then nothing should happen to the item
-    And I should be notified that quantity can not be less than 1
+    And I should be notified that a valid category must be supplied
+
+  Scenario: Failing to set the quantity of an item
+    Given that I have entered a quantity of an existing item to be less than 1
+    When I try to set the "quantity" of the item to that
+    Then nothing should happen to the item
+    And I should be notified that a quantity of 1 or more must be supplied
--- features/step_definitions/grocery-list-item.js
+++ features/step_definitions/grocery-list-item.js
@@ -1,8 +1,113 @@
 let assert = require('assert');
 let {defineSupportCode} = require('cucumber');
-let GroceryList = require('../../grocery-list.js');
+// let GroceryList = require('../../grocery-list.js');
 let GroceryListItem = require('../../grocery-list-item.js');
 
 defineSupportCode(function({Given, When, Then}) {
 
+  let randomValidName = 'valid name';
+
+  function getTestNames() { return [
+    { n: undefined},
+    { n: ''},
+    { n: [34]},
+    { n: true},
+    { n: false}
+  ]};
+
+  function getTestCategories() { return [
+    { n: 'name', c: 35},
+    { n: 'name', c: {stuff: 'hello'}},
+    { n: 'name', c: ['sta']},
+    { n: 'name', c: true},
+    { n: 'name', c: false},
+    { n: 'name', c: null}
+  ]};
+
+  function getTestQuantities(int) {
+    let r = [
+      { n: 'name', q: int-1},
+      { n: 'name', q: -int},
+      { n: 'name', q: ''},
+      { n: 'name', q: 'thirty-five'},
+      { n: 'name', q: [3]}
+    ];
+    if (int > 0) { r.push({ n: 'name', q: 0}); }
+    return r;
+  };
+
+
+  // ----         Creating a grocery list item         ----
+
+  Given('that I have not entered a valid name for a new item', function () {
+    this.testData = getTestNames();
+  });
+
+  Given('that I have not entered a valid category for a new item', function () {
+    this.testData = getTestCategories();
+  });
+
+  Given('that I have entered less than {int} as quantity for a new item', function (int) {
+    this.testData = getTestQuantities(int);
+  });
+
+
+  When('I try to create an item with that info', function () {
+    this.item = undefined;
+    for (let data of this.testData) {
+      if (this.item !== undefined) { return; }
+      try {
+        this.item = new GroceryListItem(data.n, data.c, data.q);
+      } catch(e) {}
+    }
+  });
+
+
+  Then('no item should be created', function () {
+    assert(this.item === undefined, 'an item was created with invalid arguments');
+  });
+
+  Then('I should be notified that a valid name is needed to be supplied', function () {
+    // Frontend-test
+  });
+
+  Then('I should be notified that a valid category must be supplied', function () {
+    // Frontend-test
+  });
+
+  Then('I should be notified that a quantity of {int} or more must be supplied', function (int) {
+    // Frontend-test
+  });
+
+
+  // ----         Modifying a grocery list item         ----
+
+  Given('that I have not entered a valid name for an existing item', function () {
+    this.testData = getTestNames();
+  });
+
+  Given('that I have not entered a valid category for an existing item', function () {
+    this.testData = getTestCategories();
+  });
+
+  Given('that I have entered a quantity of an existing item to be less than {int}', function (int) {
+    this.testData = getTestQuantities(int);
+  });
+
+
+  When('I try to set the {string} of the item to that', function (string) {
+    this.item = new GroceryListItem(randomValidName);
+    for (let data of this.testData) {
+      try {
+        let firstLetter = string[0]; // ex: quantity = q
+        this.item[string] = data[firstLetter]; // ex: this.item.name = data.n
+      } catch(e) {}
+    }
+  });
+
+
+  Then('nothing should happen to the item', function () {
+    let newItem = new GroceryListItem(randomValidName);
+    assert.deepStrictEqual(newItem, this.item, 'the item was changed!');
+  });
 });
