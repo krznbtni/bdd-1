f2b03d99ef7ba65657e8bc44bc8b48939ce50ebb
botanki <karzan@botani.nu>
2017-09-06 12:30:03 +0200

    Edited grocery-list.feature. Created grocery-list.js. Beginning on marking-grocery-list-item-as-bought.feature

--- features/grocery-list.feature
+++ features/grocery-list.feature
@@ -3,16 +3,18 @@ Feature: Creating one or several grocery lists
   I should be able to create one or several grocery lists
 
   Scenario: Creating a grocery list
-    Given that I fill in a valued name for a new grocery list
+    Given that I fill in a valid name for a new grocery list
     When I click the create button
     Then a grocery list should be created
 
-  Scenario: Trying to create a list with an unvalued name
-    Given that I do not fill in a valued name for a new grocery list
+  Scenario: Trying to create a list with an invalid name
+    Given that I do not fill in a valid name for a new grocery list
     When I click the create button
-    Then an error message should be shown stating that I must fill in a name for the list
+    Then a grocery list should not be created
+    And an error message should be shown stating that I must fill in a name for the list
 
   Scenario: Trying to create a list which name has already been used
-    Given that I fill in a valued name for a new grocery list that has already been used
-    When I click the click create button
-    Then an error message should be shown stating that I have already used the name before
+    Given that I fill in a valid name for a new grocery list that has already been used
+    When I click the create button
+    Then a grocery list should not be created
+    And an error message should be shown stating that I have already used the name before
---
+++ features/step_definitions/grocery-list.js
@@ -0,0 +1,59 @@
+let assert = require('assert');
+let {defineSupportCode} = require('cucumber');
+let GroceryList = require('../../grocery-list.js');
+let GroceryListItem = require('../../grocery-list-item.js');
+
+defineSupportCode(function({Given, When, Then}) {
+
+  // given = förberedelser
+  // when = utför koden. nånting händer.
+  // then = blev resultatet av When det vi förväntar oss ska hända
+
+  let usedListNames = [
+    "list-name-1",
+    "list-name-2",
+    "list-name-3",
+    "list-name-4",
+  ];
+
+  let newList;
+
+  Given('that I fill in a valid name for a new grocery list', function () {
+    this.name = 'aaa';
+  });
+
+  Given('that I do not fill in a valid name for a new grocery list', function () {
+    this.name = '';
+  });
+
+  Given('that I fill in a valid name for a new grocery list that has already been used', function () {
+    //include tittar ifall den finns i listan. default är true.
+    assert(usedListNames.includes(this.name), 'this name is already in the list');
+  });
+
+  When('I click the create button', function () {
+    this.newList = new GroceryList(this.name);
+  });
+
+  Then('a grocery list should be created', function () {
+    assert(this.newList !== undefined);
+
+    assert(this.newList.constructor.name);
+
+    //deep kollar alla variabler innanför objektet. alla variabler.
+    //strict kollar ifall det är samma variabeltyp också
+    assert.deepStrictEqual(this.newList, new GroceryList(this.name), '... successfully created the grocery list!');
+  });
+
+  Then('a grocery list should not be created', function () {
+    assert(this.newList === undefined);
+  });
+
+  Then('an error message should be shown stating that I must fill in a name for the list', function () {
+    // Frontend-test
+  });
+
+  Then('an error message should be shown stating that I have already used the name before', function () {
+    // Frontend-test
+  });
+});
