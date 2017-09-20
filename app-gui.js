1de3dc914b0178cc2d619febcaaf30e1679b5f55
DennisMajvall <dennis@majvall.se>
2017-09-20 15:47:45 +0200

    modals and stuff

--- app-gui.js
+++ app-gui.js
@@ -8,10 +8,17 @@ class AppGui {
     // define events
     this.defineMainViewEvents();
     this.defineListDetailViewEvents();
+
+    this.autoCreateAndViewAList();
+  }
+
+  autoCreateAndViewAList(){
+    let newList = new GroceryList('hej');
+    this.switchToDetailView('hej');
   }
 
   defineMainViewEvents() {
-    $(document).on('click', '#addListButton', function() {
+    $(document).on('click', '.lists #addListButton', function() {
 
       let theListName = $('#listName').val();
 
@@ -23,6 +30,8 @@ class AppGui {
                   .append('<button class="btn btn-primary btn-sm delete-list-button">delete</button>')
       );
     });
+
+    $('#add-list').on('click', ()=>this.openAddList());
 
     $(document).on('click', '.delete-list-button', function() {
       // SAVE THE SPAN TEXT
@@ -43,12 +52,67 @@ class AppGui {
       let b = $(this).prev().text();
       that.switchToDetailView(b);
     });
+
+    $(document).on('click', '.detailed #addListButton', function() {
+      let name = $('#modal #name').val();
+      that.currentList.addToList(name, $('#modal #category').val(), $('#modal #quantity').val()*1);
+
+      $('<li>')
+      .append('<span>'+name+'</span>')
+      .prependTo('#list-items');
+    });
+
+    $('#back-button').on('click', ()=>this.switchToMainView());
+    $('#add-grocery').on('click', ()=>this.openAddGrocery());
+  }
+
+  switchToMainView(){
+    $('#modal').toggleClass('lists');
+    $('#modal').toggleClass('detailed');
+
+    $('.list-detail-view').hide();
+    $('.main-view').show();
+  }
+
+  openAddGrocery(){
+    let mb = $('.modal.detailed .modal-body');
+    mb.empty();
+    mb.append(`
+      <div class="col-12">
+        Name:<br><input id="name" type="text" placeholder="Milk" autofocus>
+      </div>
+      <div class="col-12">
+        Category:<br><input id="category" type="text" placeholder="Dairies (Optional)">
+      </div>
+      <div class="col-12">
+        Quantity:<br><input id="quantity" type="number" value="1">
+      </div>
+    `);
+  }
+
+  openAddList(){
+    let mb = $('.modal.lists .modal-body');
+    mb.empty();
+    mb.append(`
+      <input placeholder="Enter list name" type="text" name="list-name" id="listName" autofocus>
+    `);
   }
 
   switchToDetailView(listName){
+    $('#modal').toggleClass('lists');
+    $('#modal').toggleClass('detailed');
+
     this.currentList = GroceryList.existingLists[listName];
     // delete GroceryList.existingLists[listName];
     // this.currentList.sortByName();
+
+    $('.list-detail-view h2').text(listName);
+
+    $('.list-items').empty();
+
+    this.currentList.items.forEach((item)=>{
+      $('.list-items').append('<span>'+item.name+'</span>');
+    });
 
     $('.main-view').hide();
     $('.list-detail-view').show();
