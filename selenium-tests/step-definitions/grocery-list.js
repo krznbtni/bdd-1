269418c62467e3e128c2d40f292a69299e52b90f
Martin Lennervald <martin.lennervald@gmail.com>
2017-09-24 13:15:50 +0200

    Add a grocery list test

--- selenium-tests/step-definitions/grocery-list.js
+++ selenium-tests/step-definitions/grocery-list.js
@@ -1,26 +1,35 @@
-module.exports = function () {
+const GroceryList = require('../../grocery-list');
 
-  this.Given(/^that I fill in a valid name for a new grocery list$/, async function () {
+module.exports = function() {
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  this.Given(/^that I fill in a valid name for a new grocery list$/, async function() {
     // Navigate to the app in the browser
     await helpers.loadPage('http://localhost:3000');
 
+    // Click addList btn
+    let addListBtn = await driver.findElement(by.css('#add-list'));
+    await addListBtn.click();
+
     // Find the input field for list name
     let inputField = await driver.findElement(by.css('#listName'));
+    await sleep(500);
 
     // Fill in some text (the name of a list)
     let listName = "Grönsaker"
     await inputField.sendKeys(listName);
+  });
 
+  this.When(/^I click the create button$/, async function() {
     // Click the button create list
     let btn = await driver.findElement(by.css('#addListButton'));
     await btn.click();
   });
 
-  this.When(/^I click the create button$/, function () {
-
-  });
-
-  this.Then(/^a grocery list should be created$/, function () {
-
+  this.Then(/^a grocery list should be created$/, async function() {
+    
   });
 }
